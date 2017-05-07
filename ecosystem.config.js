37e44c8f36834b14bf13afc7088bb5d2d5b662b0
Viktor Kenyz <vkenyz@lohika.com>
2017-05-07 13:45:53 +0300

    Update API host

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,10 +6,9 @@ module.exports = {
             ref: "origin/master",
             repo: "git@github.com:Skakruk/lodb-mobile.git",
             path: "/opt/lodb/mobile",
-            "post-deploy": "yarn install && yarn build",
+            "post-deploy": "REACT_APP_API_SERVER=https://api.lodb.org.ua yarn install && yarn build",
             env: {
-                NODE_ENV: "dev",
-                REACT_APP_API_SERVER: "https://api.lodb.org.ua"
+                NODE_ENV: "dev"
             }
         }
     }
