a825d9436b15a54b81c397936811a66723848e32
Viktor Kenyz <viktor.kenyz@thetileapp.com>
2017-12-07 12:59:46 +0200

    Fix store init

--- src/store.js
+++ src/store.js
@@ -3,8 +3,7 @@ import thunk from 'redux-thunk';
 import rootReducer from "./reducers";
 
 const enhancer = compose(
-  applyMiddleware(thunk),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  applyMiddleware(thunk)
 );
 
 export default createStore(rootReducer, {}, enhancer);
