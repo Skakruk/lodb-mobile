a9b9ae024e63266eb3f65afb09e8412d16b73ebe
Viktor Kenyz <viktor.kenyz@thetileapp.com>
2018-01-17 13:59:50 +0200

    Update info

--- src/containers/HelpYourLibrary/HelpYourLibrary.js
+++ src/containers/HelpYourLibrary/HelpYourLibrary.js
@@ -16,10 +16,16 @@ function HelpYourLibrary() {
       <p>Дякуємо вам за те, що ви є другом нашої бібліотеки!</p>
       <p>Рахунок Львівської обласної бібліотеки для дітей:</p>
       <p className={c.details}>
-        р/р <strong>31553301124404</strong><br />
-        ГУДКУ у Львівській області<br />
+        р/р <strong>31558373124404</strong><br />
+        ГУДКСУ у Львівській області<br />
         МФО <strong>825014</strong> <br />
         ЗКПО <strong>02221768</strong><br />
+      </p>
+      <p>
+        79008, м.Львів, вул.В. Винниченка, 1,<br />
+        тел. 235-87-48<br />
+        e-mail: <a href="mailto:biblioteka@lodb.org.ua">biblioteka@lodb.org.ua</a><br />
+        <a href="https://www.lodb.org.ua">www.lodb.org.ua</a>
       </p>
     </div>
   )
